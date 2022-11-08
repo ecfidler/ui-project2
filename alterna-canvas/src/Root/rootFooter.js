5dbd3ceb71dbd0583f0a3023d9ed4094a9318455
Ethan Fidler <ethancf1999@gmail.com>
2022-11-08 18:48:55 -0500

    adding basic footer

---
+++ alterna-canvas/src/Root/rootFooter.js
@@ -0,0 +1,14 @@
+function RootFooter() {
+    return (
+        <>
+            <footer>
+                <div style={{ margin: "0 1em 0 1em", textAlign: "center" }}>
+                    <p>Alterna-Canvas developed by team Flopping Fish; 2022</p>
+                    <p>User Interfaces 6067 Project 2</p>
+                </div>
+            </footer>
+        </>
+    );
+}
+
+export default RootFooter;
--- alterna-canvas/src/Root/rootLayout.js
+++ alterna-canvas/src/Root/rootLayout.js
@@ -1,13 +1,12 @@
 import { Outlet } from "react-router-dom";
+import RootFooter from "./rootFooter";
 
 function RootLayout() {
     return (
         <>
             <header>This is the Header! (it's just a placeholder)</header>
             <Outlet />
-            <footer>
-                This is the page footer (it's also just a placeholder)
-            </footer>
+            <RootFooter />
         </>
     );
 }
