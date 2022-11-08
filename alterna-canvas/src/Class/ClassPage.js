1b65b43e8c5ef3e7765e8b8ec2397642177fa938
Ethan Fidler <ethancf1999@gmail.com>
2022-11-08 15:20:32 -0500

    adding @mui/lab package

--- alterna-canvas/src/Class/ClassPage.js
+++ alterna-canvas/src/Class/ClassPage.js
@@ -1,24 +1,56 @@
+import * as React from "react";
+
 import { useParams } from "react-router-dom";
+
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import TabPanel from "@mui/lab";
+
 import data from "../metadata/unified.json";
 
 function ClassPage() {
+    const [tabValue, setValue] = React.useState(0);
     const { name } = useParams();
     const classData = data[name];
+
+    const handleTabChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     return (
         <div className="class-page">
             <h1>Class page: {name}</h1>
-            {classData.map((item, i) => {
-                return (
-                    item.type === "assignment" && (
-                        <tr key={i}>
-                            <td>{item.title}</td>
-                            <td>Due: {item.end_or_due}</td>
-                        </tr>
-                    )
-                );
-            })}
+            <Tabs
+                orientation="vertical"
+                variant="fullWidth"
+                value={tabValue}
+                onChange={handleTabChange}
+                aria-label="Vertical tabs example"
+                sx={{ borderRight: 1, borderColor: "divider" }}
+            >
+                <Tab label="Assignments" value="assignments" />
+                <Tab label="Announcements" value="annoucements" />
+                <Tab label="Grades" value="grades" />
+                <Tab label="Course Materials" value="course-materials" />
+                <Tab label="Syllabus" value="syllabus" />
+                <Tab label="Zoom" value="zoom" />
+            </Tabs>
         </div>
     );
 }
 
 export default ClassPage;
+
+/* List all assignments code example
+{classData.map((item, i) => {
+    return (
+        item.type === "assignment" && (
+            <tr key={i}>
+                <td>{item.title}</td>
+                <td>Due: {item.end_or_due}</td>
+            </tr>
+        )
+    );
+})}
+
+*/
