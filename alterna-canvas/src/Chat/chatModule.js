39a703a709e330194d57c214acf984466e603c74
Quiltic <jbot237@gmail.com>
2022-11-12 17:44:54 -0500

    Fixed bug with scrolling, added chat button

--- alterna-canvas/src/Chat/chatModule.js
+++ alterna-canvas/src/Chat/chatModule.js
@@ -1,4 +1,5 @@
 
+import { maxHeight } from '@mui/system';
 import {useRef, useState} from 'react';
 
 
@@ -8,6 +9,7 @@ function ChatModule({data}) {
 
     const inputRef = useRef(null);
     const mainBody = useRef(null);
+    const bottom = useRef(null);
 
 
     const clickPress = (event) => {
@@ -18,54 +20,63 @@ function ChatModule({data}) {
 
     const updateChatWindow = () => {
         let tempText = inputRef.current.value;
+        if (tempText != "") {
 
-        let msg = {user:"Me", avatar:null, content:tempText, file:null};
-        setData([...chatData, msg]);
+            let msg = {user:"Me", avatar:null, content:tempText, file:null};
+            setData([...chatData, msg]);
 
-        // console.log(tempText);
-        inputRef.current.value = ""; // reset text field
-        // console.log(chatData)
+            // console.log(tempText);
+            inputRef.current.value = ""; // reset text field
+            // console.log(chatData)
+            // bottom.current.scrollIntoView();
+        }
     }
 
 
 
     return (
-        <div>
-        <div style={{ padding: "5px", overflowY:"auto", display: "flex", flexDirection: "column", alignItems:"flex-start"}} ref={mainBody}>
-            {
-                chatData.map((data, index) => {
-                    let wordStyle = {backgroundColor:"silver", borderRadius:"10px", marginBottom: "4px", maxWidth:"100%"}
-                    if (data.user == "Me") {
-                        wordStyle.alignSelf = "flex-end"
+        <div style={{height:"100%"}}>
+            <div style={{overflowY:"auto", maxHeight:"84%"}}>
+                <div style={{ padding: "5px", display: "flex", flexDirection: "column", alignItems:"flex-start", maxHeight:"90%"}} ref={mainBody}>
+                    {
+                        chatData.map((data, index) => {
+                            let wordStyle = {backgroundColor:"silver", borderRadius:"10px", marginBottom: "4px", maxWidth:"100%"}
+                            if (data.user == "Me") {
+                                wordStyle.alignSelf = "flex-end"
+                            }
+                            return (
+                                <div style={wordStyle} key={index} >
+                                    <p style={{fontSize:"15px", color:"grey", margin:"5px 10px 5px 10px", wordBreak:"break-all", overflowWrap:"break-word"}}>{data.user}</p>
+                                    <p style={{margin:"0px 10px 10px 10px"}}>{data.content}</p>
+                                </div>
+                            )
+                        })
+                        
+                        // console.log(chatData.at(-1))//.scrollIntoView()
                     }
-                    return (
-                        <div style={wordStyle}>
-                            <p style={{fontSize:"15px", color:"grey", margin:"5px 10px 5px 10px", wordBreak:"break-all", overflowWrap:"break-word"}}>{data.user}</p>
-                            <p key={index} style={{margin:"0px 10px 10px 10px"}}>{data.content}</p>
-                        </div>
-                    )
-                })
-            }
-        </div>
-        <div
-            style={{
-                width: "100%",
-                height: "10%",
-                maxHeight:"50px",
-                minHeight:"20px",
-                backgroundColor: "silver",
-                color: "white",
-                textAlign: "left",
-                borderRadius: "2px",
-                position: "absolute",
-                bottom: "0",
-            }}
-        >
-            <button style={{ float: "left-middle" }}>+</button>
-            {/* <input type="file" name="input-file" style={{ float: "left-middle" }}>+</input> */}
-            <input type="text" name="input-text" placeholder="Text" onKeyPress={(event) => clickPress(event)} ref={inputRef} style={{width:"60%", display:"inline"}}></input>
-            <button style={{ float: "right-middle" }} onClick={event => updateChatWindow()}>-></button>
-        </div>
+                    <span style={{height:"0px"}} ref={bottom}></span>
+                </div>
+            </div>
+            <div
+                style={{
+                    width: "100%",
+                    height: "10%",
+                    maxHeight:"50px",
+                    minHeight:"20px",
+                    backgroundColor: "silver",
+                    color: "white",
+                    textAlign: "left",
+                    borderRadius: "2px",
+                    position: "absolute",
+                    bottom: "0",
+                    
+                }}
+            >
+                <button style={{ float: "left-middle" }}>+</button>
+                {/* <input type="file" name="input-file" style={{ float: "left-middle" }}>+</input> */}
+                <input type="text" name="input-text" placeholder="Text" onKeyPress={(event) => clickPress(event)} ref={inputRef} style={{width:"60%", display:"inline"}}></input>
+                <button style={{ float: "right-middle" }} onClick={event => updateChatWindow()}>-></button>
+            </div>
         </div>
             
     );
--- alterna-canvas/src/Chat/chatMoveable.js
+++ alterna-canvas/src/Chat/chatMoveable.js
@@ -2,13 +2,41 @@
 import Draggable from "react-draggable";
 import ChatModule from './chatModule';
 
+import Button from "@mui/material/Button";
+import {useRef, useState} from 'react';
+
 
 function ChatMoveable() {
 
     let data = [{user:"Etan", avatar:null, content:"Hellow, worlds", file:null}, {user:"Me", avatar:null, content:"This sorta wor...", file:null}];
     
+    const moveable = useRef(null);
+    const bubble = useRef(null);
+
+    function hideMove() {
+        moveable.current.style.display = "none";
+        bubble.current.style.display = "block";
+    }
+    function hideBubble() {
+        bubble.current.style.display = "none";
+        moveable.current.style.display = "block";
+    }
+
+
     return (
-        <Draggable handle="#handle">
+        <div>
+            <Button
+            variant="contained"
+            size="large"
+            onClick={hideBubble}
+            // startIcon={<DownloadIcon />}
+            ref={bubble}
+        >
+           Chat
+        </Button>
+        {/* <div ref={bubble} style={{backgroundColor:"red", color:"white", borderRadius:"10px", width:"100px", float:"left"}} onclick={event => hideBubble()}>Chat</div> */}
+
+        <Draggable handle="#handle" >
             <div
                 className="box"
                 style={{
@@ -18,10 +46,12 @@ function ChatMoveable() {
                     borderRadius: "5px",
                     backgroundColor: "white",
                     resize: "both",
-                    overflow: "auto",
+                    overflow: "hidden",
                     minHeight:"300px",
                     minWidth:"200px",
+                    display:"none",
                 }}
+                ref={moveable}
             >
                 <div
                     id="handle"
@@ -46,13 +76,14 @@ function ChatMoveable() {
                     >
                         Mysc words as a title
                     </h1>
-                    <button style={{ float: "right" }}>X</button>
+                    <button style={{ float: "right" }}  onClick={event => hideMove()}>X</button>
                 </div>
                 
                 <ChatModule data={data}/>
 
             </div>
         </Draggable>
+        </div>
     );
 }
 
