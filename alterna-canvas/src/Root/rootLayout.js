d5ee7271ddcd8055f0d31008d0438f7fa6333fd6
Nishanth <chidamnm@mail.uc.edu>
2022-11-14 18:35:38 -0500

    style header

--- alterna-canvas/src/Root/rootLayout.js
+++ alterna-canvas/src/Root/rootLayout.js
@@ -6,10 +6,12 @@ import RootFooter from "./rootFooter";
 function RootLayout() {
     return (
         <>
-            <h1 style={{backgroundColor: '#e00122', color: 'white', marginLeft: 'auto', fontSize: '75px', textAlign: 'center'}}>
-                <img src="UCicon.png" alt="UC logo" width="100" height="100" style={{top:"50%"}} />
-                Alterna-Canvas
-            </h1>
+            <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', backgroundColor: '#e00122', color: 'white'}}>
+                <img src="UCicon.png" alt="UC logo" width="100" height="100" style={{justifyContent:"flex-start"}} />
+                <h1 style={{fontSize: '75px'}}>
+                    Alterna-Canvas
+                </h1>
+            </div>
             <Outlet />
             <RootFooter />
         </>
