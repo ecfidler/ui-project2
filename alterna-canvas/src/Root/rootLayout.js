1e79c1667f5b2af5e2a61e3c993ecfdb7bf2648b
Quiltic <jbot237@gmail.com>
2022-11-14 19:02:34 -0500

    header was TOO BIG

--- alterna-canvas/src/Root/rootLayout.js
+++ alterna-canvas/src/Root/rootLayout.js
@@ -7,8 +7,8 @@ function RootLayout() {
     return (
         <>
             <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', backgroundColor: '#e00122', color: 'white'}}>
-                <img src="UCicon.png" alt="UC logo" width="100" height="100" style={{justifyContent:"flex-start"}} />
-                <h1 style={{fontSize: '75px'}}>
+                <img src="UCicon.png" alt="UC logo" width="60" height="60" style={{justifyContent:"flex-start"}} />
+                <h1 style={{fontSize: '40px'}}>
                     Alterna-Canvas
                 </h1>
             </div>
